e5a5c6579fb072af24ea13ab41012166718b4e30
kantemirigor <kantemirigor@gmail.com>
2023-05-10 02:48:11 +0300

    teteet

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
-const initialCards = [
-{
-  name: 'Архыз',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-},
-{
-  name: 'Челябинская область',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-},
-{
-  name: 'Иваново',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-},
-{
-  name: 'Камчатка',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-},
-{
-  name: 'Холмогорский район',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-},
-{
-  name: 'Байкал',
-  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-}
-];
 const elements = document.querySelector('.elements');
 const tempElement = document.querySelector('#temp-element').content;
 const popupProfile = document.querySelector('.popup');
 const buttonEdit = document.querySelector('.profile__button-edit');
-const buttonClose = document.querySelectorAll('.popup__close-button');
+const buttonCloseProfile = document.querySelector("#close-edit-popup");
+const buttonCloseMesto = document.querySelector("#close-mesto-popup");
+const buttonCloseImage = document.querySelector("#close-image-popup");
 const buttonSubmit = document.querySelectorAll('.popup__submit-button');
 const inputName =  document.querySelector('#popup__input-name');
 const inputDescription =  document.querySelector('#popup__input-description');
@@ -37,54 +13,64 @@ const profileDescription = document.querySelector('.profile__description');
 const buttonAdd = document.querySelector('.profile__button-add');
 const popupMesto = document.querySelector('.popup-mesto');
 const popupImage = document.querySelector('.popup-image');
+const formElementEdit = document.querySelector('#popup-edit');
+const formElementMesto = document.querySelector('#popup-mesto');
+const mestoUrl = document.querySelector('#popup__input-link');
+const mestoTitle = document.querySelector('#popup__input-title');
+const popupBigImage = document.querySelector('.popup__image');
+const popupBigImageTitle = document.querySelector('.popup__image-title');
 
 initialCards.forEach(function(elem){
-  console.log(elem.link)
-  const element = tempElement.querySelector('.element').cloneNode(true); // дубль с 81 строкой 
-  element.querySelector('.element__image').src = elem.link;
-  element.querySelector('.element__text').textContent = elem.name;
+  const element = createElement(elem.link,elem.name);
   elements.append(element);
 })
 
-buttonEdit.addEventListener('click',showPopup);
-buttonClose[0].addEventListener('click',closePopup);  // Исправить!
-buttonClose[1].addEventListener('click',closePopup);  // Исправить!
-buttonClose[2].addEventListener('click',closePopup);  // Исправить!
-buttonSubmit[0].addEventListener('click', saveProfile); // Исправить!
-buttonSubmit[1].addEventListener('click', saveMesto); // Исправить!
-buttonAdd.addEventListener('click' , addMesto);
 
-function showPopup(){
-  popupProfile.classList.add('popup_opened'); //Можно совместить с 77?
+buttonEdit.addEventListener('click',editPopupProfile);
+buttonAdd.addEventListener('click',openPopup(popupMesto));
+buttonCloseProfile.addEventListener('click',closePopup(popupProfile));
+buttonCloseMesto.addEventListener('click',closePopup(popupMesto));
+buttonCloseImage.addEventListener('click',closePopup(popupImage));
+formElementEdit.addEventListener('submit',saveProfile);
+formElementMesto.addEventListener('submit',saveMesto);
+
+function createElement(url,title){
+  const element = tempElement.querySelector('.element').cloneNode(true);
+  element.querySelector('.element__image').src = url;
+  element.querySelector('.element__image').alt = title;
+  element.querySelector('.element__text').textContent = title;
+  return element;
+}
+function openPopup(popup){
+  return function () {
+    popup.classList.add('popup_opened');
+  }
+}
+
+function editPopupProfile(){
+  openPopup(popupProfile)();
   inputName.value = profileTitle.textContent;
   inputDescription.value = profileDescription.textContent;
-  
 }
-function closePopup(){
-  popupProfile.classList.remove('popup_opened');
-  popupMesto.classList.remove('popup_opened');
-  popupImage.classList.remove('popup_opened');
+function closePopup(popup){
+  return function () {
+    popup.classList.remove('popup_opened');
+  }
 }
 function saveProfile(evt){
   evt.preventDefault();
   profileTitle.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
-}
-function addMesto(){
-  popupMesto.classList.add('popup_opened');  // Можно совместить с 77?
-}
-function fullImage(){
-  popupImage.classList.add('popup_opened');
+  closePopup(popupProfile)();
 }
 function saveMesto(evt){
   evt.preventDefault();
-  const element = tempElement.querySelector('.element').cloneNode(true);  // дубль с 43 строкой
-  element.querySelector('.element__image').src = document.querySelector('#popup__input-link').value;
-  element.querySelector('.element__text').textContent = document.querySelector('#popup__input-title').value;
+  const element = createElement(mestoUrl.value,mestoTitle.value)
   elements.prepend(element); 
+  closePopup(popupMesto)();
 }
 elements.onclick = function(event) {
-  let target = event.target;
+  const target = event.target;
   if(target.id === 'like')
   {
     target.classList.toggle('element__like_active')
@@ -95,8 +81,8 @@ elements.onclick = function(event) {
   }
   if(target.id === 'element-image')
   {
-    document.querySelector('.popup__image').src = target.src;
-    document.querySelector('.popup__image-title').textContent = target.nextElementSibling.firstElementChild.textContent;
-    fullImage();
+    popupBigImage.src = target.src;
+    popupBigImageTitle.textContent = target.nextElementSibling.firstElementChild.textContent;
+    openPopup(popupImage)();
   }
 };
