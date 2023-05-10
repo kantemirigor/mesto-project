9f907cf868c450ff91d1c790f8ccf08dd544603a
kantemirigor <kantemirigor@gmail.com>
2023-05-10 12:49:00 +0300

    changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const elements = document.querySelector('.elements');
 const tempElement = document.querySelector('#temp-element').content;
-const popupProfile = document.querySelector('.popup');
-const buttonEdit = document.querySelector('.profile__button-edit');
+const popupProfile = document.querySelector('.popup-profile');
+const buttonOpenPopupEditProfile = document.querySelector('.profile__button-edit');
 const buttonCloseProfile = document.querySelector("#close-edit-popup");
-const buttonCloseMesto = document.querySelector("#close-mesto-popup");
+const buttonClosePopupAddCard = document.querySelector("#close-mesto-popup");
 const buttonCloseImage = document.querySelector("#close-image-popup");
 const buttonSubmit = document.querySelectorAll('.popup__submit-button');
 const inputName =  document.querySelector('#popup__input-name');
 const inputDescription =  document.querySelector('#popup__input-description');
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
-const buttonAdd = document.querySelector('.profile__button-add');
-const popupMesto = document.querySelector('.popup-mesto');
+const buttonOpenPopupAddCard = document.querySelector('.profile__button-add');
+const popupAddCard = document.querySelector('.popup-mesto');
 const popupImage = document.querySelector('.popup-image');
 const formElementEdit = document.querySelector('#popup-edit');
 const formElementMesto = document.querySelector('#popup-mesto');
@@ -26,48 +26,59 @@ initialCards.forEach(function(elem){
 })
 
 
-buttonEdit.addEventListener('click',editPopupProfile);
-buttonAdd.addEventListener('click',openPopup(popupMesto));
-buttonCloseProfile.addEventListener('click',closePopup(popupProfile));
-buttonCloseMesto.addEventListener('click',closePopup(popupMesto));
-buttonCloseImage.addEventListener('click',closePopup(popupImage));
-formElementEdit.addEventListener('submit',saveProfile);
-formElementMesto.addEventListener('submit',saveMesto);
+buttonOpenPopupEditProfile.addEventListener('click',editPopupProfile);
+buttonOpenPopupAddCard.addEventListener('click',function()
+{
+  openPopup(popupAddCard);
+});
+buttonCloseProfile.addEventListener('click',function(){
+  closePopup(popupProfile);
+});
+buttonClosePopupAddCard.addEventListener('click',function()
+{
+  closePopup(popupAddCard);
+});
+buttonCloseImage.addEventListener('click',function(){
+  closePopup(popupImage);
+});
+formElementEdit.addEventListener('submit',submitSaveProfile);
+formElementMesto.addEventListener('submit',submitAddCardForm);
 
 function createElement(url,title){
   const element = tempElement.querySelector('.element').cloneNode(true);
-  element.querySelector('.element__image').src = url;
-  element.querySelector('.element__image').alt = title;
+  const elementImage = element.querySelector('.element__image');
+  elementImage.src = url;
+  elementImage.alt = title;
   element.querySelector('.element__text').textContent = title;
   return element;
 }
 function openPopup(popup){
-  return function () {
     popup.classList.add('popup_opened');
-  }
 }
 
 function editPopupProfile(){
-  openPopup(popupProfile)();
+  openPopup(popupProfile);
   inputName.value = profileTitle.textContent;
   inputDescription.value = profileDescription.textContent;
 }
 function closePopup(popup){
-  return function () {
     popup.classList.remove('popup_opened');
-  }
 }
-function saveProfile(evt){
+function submitSaveProfile(evt){
   evt.preventDefault();
   profileTitle.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
+  inputName.value = '';
+  inputDescription.value = '';
   closePopup(popupProfile)();
 }
-function saveMesto(evt){
+function submitAddCardForm(evt){
   evt.preventDefault();
   const element = createElement(mestoUrl.value,mestoTitle.value)
-  elements.prepend(element); 
-  closePopup(popupMesto)();
+  elements.prepend(element);
+  mestoUrl.value = '';
+  mestoTitle.value = ''; 
+  closePopup(popupAddCard)();
 }
 elements.onclick = function(event) {
   const target = event.target;
@@ -82,7 +93,8 @@ elements.onclick = function(event) {
   if(target.id === 'element-image')
   {
     popupBigImage.src = target.src;
+    popupBigImage.alt = target.nextElementSibling.firstElementChild.textContent;
     popupBigImageTitle.textContent = target.nextElementSibling.firstElementChild.textContent;
-    openPopup(popupImage)();
+    openPopup(popupImage);
   }
 };
